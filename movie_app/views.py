b0a4f3a4b14071b8f48811cef83fb9060ef9eab0
DIMA\turch <turchinskiyd@gmail.com>
2023-07-12 20:22:29 +0300

    Initial commit

---
+++ movie_app/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Movie
+
+
+# Create your views here.
+
+def show_all_movie(request):
+    movies = Movie.objects.all()
+    return render(request, 'movie_app/all_movies.html',
+                  {'movies': movies})
+
+
+def show_one_movie(request, id_movie: int):
+    # movie = Movie.objects.get(id=id_movie)
+    movie = get_object_or_404(Movie,id=id_movie)
+    return render(request, 'movie_app/one_movie.html',
+                  {'movie': movie})
