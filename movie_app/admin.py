1569901bef1a09856afd0a37c7df936098b26475
DIMA\turch <turchinskiyd@gmail.com>
2023-07-15 18:45:45 +0300

    add choice field, admin action, search and filter

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,19 +1,41 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
 from django.db.models import QuerySet
 
+class RatingFilter(admin.SimpleListFilter):
+    title = 'Фільтр по рейтингу'
+    parameter_name = 'rating'
+    def lookups(self, request, model_admin):
+        return [
+            ('<40', 'Низький'),
+            ('від 40 до 59', 'Середній'),
+            ('від 60 до 79', 'Високий'),
+            ('>=80', 'Найвищий'),
+        ]
+    def queryset(self, request, queryset:QuerySet):
+        if self.value() == '<40':
+            return queryset.filter(rating__lt=40)
+        if self.value() == 'від 40 до 59':
+            return queryset.filter(rating__gte=40).filter(rating__lt=60)
+        if self.value() == 'від 60 до 79':
+            return queryset.filter(rating__gte=60).filter(rating__lt=80)
+        if self.value() == '>=80':
+            return queryset.filter(rating__gte=80)
+        return queryset
 
-# Register your models here.
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
-    list_editable = ['rating', 'year', 'budget']
+    list_display = ['name', 'rating', 'currency', 'budget', 'rating_status']
+    list_editable = ['rating', 'currency', 'budget']
     ordering = ['-rating', '-name']
     list_per_page = 10
+    actions = ['set_dollars', 'set_euro']
+    search_fields = ['name', 'rating']
+    list_filter = ['name', 'currency', RatingFilter]
 
     @admin.display(ordering='rating', description='Статус')
-    def rating_status(self, mov:Movie):
+    def rating_status(self, mov: Movie):
         if mov.rating < 50:
             return 'Навіщо таке дивитися'
         if mov.rating < 70:
@@ -22,4 +44,15 @@ class MovieAdmin(admin.ModelAdmin):
             return 'Годиться'
         return 'Топчик'
 
+    @admin.action(description='Встановити валюту в Долар')
+    def set_dollars(self, request, qs: QuerySet):
+        qs.update(currency=Movie.USD)
 
+    @admin.action(description='Встановити валюту в Євро')
+    def set_euro(self, request, qs: QuerySet):
+        count_update = qs.update(currency=Movie.EUR)
+        self.message_user(
+            request,
+            f'Було оновлено {count_update} записи',
+            messages.ERROR
+        )
--- movie_app/models.py
+++ movie_app/models.py
@@ -6,10 +6,20 @@ from django.utils.text import slugify
 # Create your models here.
 
 class Movie(models.Model):
+    EUR = "EUR"
+    USD = "USD"
+    UAH = "UAH"
+    CURRENCY_CHOICES = [
+        (EUR, "Euro"),
+        (USD, "Dollar"),
+        (UAH, "Hryvnia")
+    ]
+
     name = models.CharField(max_length=40)
     rating = models.IntegerField()
     year = models.IntegerField(null=True, blank=True)
     budget = models.IntegerField(default=1000000)
+    currency = models.CharField(max_length=3, choices=CURRENCY_CHOICES, default=UAH)
     slug = models.SlugField(default='', null=False)
 
     def save(self, *args, **kwargs):
