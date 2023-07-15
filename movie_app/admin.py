208aab12015bd73f959f78a5732cb8b87295cba3
DIMA\turch <turchinskiyd@gmail.com>
2023-07-15 17:30:20 +0300

    add admin panel

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Movie
+from django.db.models import QuerySet
+
 
 # Register your models here.
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
+    list_editable = ['rating', 'year', 'budget']
+    ordering = ['-rating', '-name']
+    list_per_page = 10
+
+    @admin.display(ordering='rating', description='Статус')
+    def rating_status(self, mov:Movie):
+        if mov.rating < 50:
+            return 'Навіщо таке дивитися'
+        if mov.rating < 70:
+            return 'Один раз можна й глянути'
+        if mov.rating <= 85:
+            return 'Годиться'
+        return 'Топчик'
+
+
--- movie_proj/urls.py
+++ movie_proj/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
+admin.site.site_header = 'Адмінка Джанго'
+admin.site.index_title = 'Наша адмінка'
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('movie_app.urls')),
